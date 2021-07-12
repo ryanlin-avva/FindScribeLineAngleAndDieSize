1c67576ee207b4092bffe7cd838283e47701629a
snow-avva <Snow_Chen@avvatech.com>
2021-07-12 08:56:31 +0800

    Revert "modify"
    This reverts commit 6f268c17053223d694ff7224d18af79a5eafef82.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -50,7 +50,7 @@
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.tbOffset = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -65,9 +65,10 @@
             // 
             this.pictureBox1.ContextMenuStrip = this.contextMenuStrip1;
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 28);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1418, 592);
+            this.pictureBox1.Size = new System.Drawing.Size(1924, 851);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
@@ -79,12 +80,12 @@
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.SaveImageToolStripMenuItem});
             this.contextMenuStrip1.Name = "ContextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(68, 26);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(70, 26);
             // 
             // SaveImageToolStripMenuItem
             // 
             this.SaveImageToolStripMenuItem.Name = "SaveImageToolStripMenuItem";
-            this.SaveImageToolStripMenuItem.Size = new System.Drawing.Size(67, 22);
+            this.SaveImageToolStripMenuItem.Size = new System.Drawing.Size(69, 22);
             // 
             // menuStrip1
             // 
@@ -103,89 +104,90 @@
             this.laplaceToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(2, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(1418, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1924, 28);
             this.menuStrip1.TabIndex = 4;
             this.menuStrip1.Text = "MenuStrip1";
             // 
             // OpenToolStripMenuItem
             // 
             this.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem";
-            this.OpenToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.OpenToolStripMenuItem.Size = new System.Drawing.Size(61, 24);
             this.OpenToolStripMenuItem.Text = "Open";
             this.OpenToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
             // 
             // OutlineToolStripMenuItem
             // 
             this.OutlineToolStripMenuItem.Name = "OutlineToolStripMenuItem";
-            this.OutlineToolStripMenuItem.Size = new System.Drawing.Size(133, 20);
+            this.OutlineToolStripMenuItem.Size = new System.Drawing.Size(163, 24);
             this.OutlineToolStripMenuItem.Text = "Outline(Connection)";
-            this.OutlineToolStripMenuItem.Click += new System.EventHandler(this.OutlineMenuItem_Click);
+            this.OutlineToolStripMenuItem.Click += new System.EventHandler(this.OutlineToolStripMenuItem_Click);
             // 
             // TargetsToolStripMenuItem
             // 
             this.TargetsToolStripMenuItem.Name = "TargetsToolStripMenuItem";
-            this.TargetsToolStripMenuItem.Size = new System.Drawing.Size(110, 20);
+            this.TargetsToolStripMenuItem.Size = new System.Drawing.Size(135, 24);
             this.TargetsToolStripMenuItem.Text = "Targets(Region)";
             this.TargetsToolStripMenuItem.Click += new System.EventHandler(this.TargetsToolStripMenuItem_Click);
             // 
             // FilterToolStripMenuItem
             // 
             this.FilterToolStripMenuItem.Name = "FilterToolStripMenuItem";
-            this.FilterToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.FilterToolStripMenuItem.Size = new System.Drawing.Size(58, 24);
             this.FilterToolStripMenuItem.Text = "Filter";
             this.FilterToolStripMenuItem.Click += new System.EventHandler(this.FilterToolStripMenuItem_Click);
             // 
             // regionMarkToolStripMenuItem
             // 
             this.regionMarkToolStripMenuItem.Name = "regionMarkToolStripMenuItem";
-            this.regionMarkToolStripMenuItem.Size = new System.Drawing.Size(97, 20);
+            this.regionMarkToolStripMenuItem.Size = new System.Drawing.Size(119, 24);
             this.regionMarkToolStripMenuItem.Text = "Region(Mark)";
-            this.regionMarkToolStripMenuItem.Click += new System.EventHandler(this.RegionMarkMenuItem_Click);
+            this.regionMarkToolStripMenuItem.Click += new System.EventHandler(this.regionMarkToolStripMenuItem_Click);
             // 
             // scribeToolStripMenuItem
             // 
             this.scribeToolStripMenuItem.Name = "scribeToolStripMenuItem";
-            this.scribeToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.scribeToolStripMenuItem.Size = new System.Drawing.Size(95, 24);
             this.scribeToolStripMenuItem.Text = "ScribeLine";
             this.scribeToolStripMenuItem.Click += new System.EventHandler(this.scribeToolStripMenuItem_Click);
             // 
             // equalToolStripMenuItem
             // 
             this.equalToolStripMenuItem.Name = "equalToolStripMenuItem";
-            this.equalToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.equalToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
             this.equalToolStripMenuItem.Text = "Equal";
             this.equalToolStripMenuItem.Click += new System.EventHandler(this.equalToolStripMenuItem_Click);
             // 
             // BinaryToolStripMenuItem
             // 
             this.BinaryToolStripMenuItem.Name = "BinaryToolStripMenuItem";
-            this.BinaryToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.BinaryToolStripMenuItem.Size = new System.Drawing.Size(67, 24);
             this.BinaryToolStripMenuItem.Text = "Binary";
             this.BinaryToolStripMenuItem.Click += new System.EventHandler(this.BinaryToolStripMenuItem_Click);
             // 
             // laplaceToolStripMenuItem
             // 
             this.laplaceToolStripMenuItem.Name = "laplaceToolStripMenuItem";
-            this.laplaceToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.laplaceToolStripMenuItem.Size = new System.Drawing.Size(75, 24);
             this.laplaceToolStripMenuItem.Text = "Laplace";
             this.laplaceToolStripMenuItem.Click += new System.EventHandler(this.laplaceToolStripMenuItem_Click);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 12;
-            this.listBox1.Location = new System.Drawing.Point(621, 0);
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(828, 0);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
             this.listBox1.Name = "listBox1";
             this.listBox1.ScrollAlwaysVisible = true;
-            this.listBox1.Size = new System.Drawing.Size(306, 28);
+            this.listBox1.Size = new System.Drawing.Size(407, 34);
             this.listBox1.TabIndex = 6;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(1019, 2);
+            this.textBox1.Location = new System.Drawing.Point(1359, 2);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(30, 22);
+            this.textBox1.Size = new System.Drawing.Size(39, 25);
             this.textBox1.TabIndex = 7;
             this.textBox1.Text = "3";
             // 
@@ -193,17 +195,19 @@
             // 
             this.panel1.Controls.Add(this.radioButton2);
             this.panel1.Controls.Add(this.radioButton1);
-            this.panel1.Location = new System.Drawing.Point(1126, 0);
+            this.panel1.Location = new System.Drawing.Point(1501, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(105, 28);
+            this.panel1.Size = new System.Drawing.Size(140, 35);
             this.panel1.TabIndex = 8;
             // 
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(53, 6);
+            this.radioButton2.Location = new System.Drawing.Point(71, 8);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(47, 16);
+            this.radioButton2.Size = new System.Drawing.Size(58, 19);
             this.radioButton2.TabIndex = 1;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "白底";
@@ -212,9 +216,10 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(0, 6);
+            this.radioButton1.Location = new System.Drawing.Point(0, 8);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(47, 16);
+            this.radioButton1.Size = new System.Drawing.Size(58, 19);
             this.radioButton1.TabIndex = 0;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "黑底";
@@ -222,69 +227,75 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(1296, 0);
+            this.textBox2.Location = new System.Drawing.Point(1728, 0);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(33, 22);
+            this.textBox2.Size = new System.Drawing.Size(43, 25);
             this.textBox2.TabIndex = 9;
             this.textBox2.Text = "8";
             // 
-            // tbOffset
+            // textBox3
             // 
-            this.tbOffset.Location = new System.Drawing.Point(1382, 0);
-            this.tbOffset.Name = "tbOffset";
-            this.tbOffset.Size = new System.Drawing.Size(36, 22);
-            this.tbOffset.TabIndex = 10;
-            this.tbOffset.Text = "8";
-            this.tbOffset.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.Location = new System.Drawing.Point(1843, 0);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(47, 25);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.Text = "8";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1055, 9);
+            this.label1.Location = new System.Drawing.Point(1407, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.Size = new System.Drawing.Size(82, 15);
             this.label1.TabIndex = 11;
             this.label1.Text = "二值化基底";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(933, 8);
+            this.label2.Location = new System.Drawing.Point(1244, 10);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 12);
+            this.label2.Size = new System.Drawing.Size(79, 15);
             this.label2.TabIndex = 12;
             this.label2.Text = "binary offset";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(1335, 6);
+            this.label3.Location = new System.Drawing.Point(1780, 8);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.Size = new System.Drawing.Size(52, 15);
             this.label3.TabIndex = 13;
             this.label3.Text = "亮度差";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(1237, 6);
+            this.label4.Location = new System.Drawing.Point(1649, 8);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.Size = new System.Drawing.Size(67, 15);
             this.label4.TabIndex = 14;
             this.label4.Text = "方塊大小";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(1028, 633);
+            this.ClientSize = new System.Drawing.Size(1924, 879);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.tbOffset);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.textBox1);
@@ -292,6 +303,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Get Targets";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -323,7 +335,7 @@
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox tbOffset;
+        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.ToolStripMenuItem equalToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem laplaceToolStripMenuItem;
         private System.Windows.Forms.Label label1;
